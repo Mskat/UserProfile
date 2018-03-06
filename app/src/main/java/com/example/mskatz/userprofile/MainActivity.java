42d0ad70ff2ae1d7190d5d9f8e7ec1a168bc02e0
mskatz <zur.katarzyna@gmail.com>
2018-03-06 18:19:55 +0100

    1.03 - Changed fonts, text color and size

--- app/src/main/java/com/example/mskatz/userprofile/MainActivity.java
+++ app/src/main/java/com/example/mskatz/userprofile/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.mskatz.userprofile;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -18,11 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
         TextView name = findViewById(R.id.name);
         name.setText("Kate");
+        name.setTextSize(35);
+        name.setTypeface(Typeface.SANS_SERIF, Typeface.ITALIC);
+        name.setTextColor(Color.parseColor("#cc3300"));
 
         TextView birthday = findViewById(R.id.birthday);
-        birthday.setText("19.04.1991");
+        birthday.setText("19/04/1991");
+        birthday.setTextSize(20);
+        name.setTypeface(Typeface.SANS_SERIF, Typeface.ITALIC);
+        birthday.setTextColor(Color.parseColor("#cc3300"));
 
         TextView country = findViewById(R.id.country);
         country.setText("Poland");
+        country.setTextSize(20);
+        name.setTypeface(Typeface.SANS_SERIF, Typeface.ITALIC);
+        country.setTextColor(Color.parseColor("#cc3300"));
     }
 }
